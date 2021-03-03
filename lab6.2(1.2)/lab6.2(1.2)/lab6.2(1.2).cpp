c3c367aeed56e38796d731b2e08d43aaf1187da9
mityamuch <much.mitya@gmail.com>
2021-03-04 00:18:36 +0300

    base published

---
+++ lab6.2(1.2)/lab6.2(1.2)/lab6.2(1.2).cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <memory.h>
+#include <map>
+using namespace std;
+//класс Monom
+class Monom {
+private:
+    map<char, int> letters;
+    int k;
+public:
+    Monom()=default; //конструктор без параметров
+    Monom(char* equation) {
+        int l=sscanf(equation, "%d", k);
+       
+     
+    }//конструктор с параметрами
+    Monom(const Monom& m) {
+        k = m.k;
+        letters = m.letters;
+    }//копирующий конструктор
+    ~Monom(); //деструктор
+
+    Monom operator= (const Monom& m);   //оператор сложения
+    Polynom operator+ (const Monom& m);//оператор присваивания
+    Polynom operator+= (const Monom& m);
+    Polynom operator- (const Monom& m);
+    Polynom operator-= (const Monom& m);
+    Monom operator* (const Monom& m);
+    Monom operator*= (const Monom& m);
+    Monom operator/ (char dif);
+    Monom operator/= (char dif);
+    Monom operator== (const Monom& m);
+    Monom operator!= (const Monom& m);
+    friend ostream& operator<<(ostream& stream, const Monom& m);
+    friend istream& operator>>(istream& stream,Monom& m);
+};
+
+
