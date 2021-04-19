c86ad8a3e466cd6e9dd9c1102a6f6f375fa1f03a
mityamuch <74504728+mityamuch@users.noreply.github.com>
2021-04-19 14:46:52 +0300

    red_black preready

--- lab7/red_black_tree.h
+++ lab7/red_black_tree.h
@@ -1,262 +1,407 @@
 ﻿#pragma once
-#include<iostream>
+
 #include "binary_tree.h"
 
-
 template <typename T>
-class RBTree
+class   Red_black_tree : public binary_tree<T>
 {
+private:
+    enum            color { RED, BLACK };
+
+    struct          tree_node
+    {
+        T           data;
+        int         color;
+        tree_node* left;
+        tree_node* right;
+        tree_node* parent;
+
+        //
+    };
 
 private:
-	struct TreeStruct {
-		int info;
-		T key;
-		bool black; //True - черное 
-		TreeStruct* right;
-		TreeStruct* left;
-		TreeStruct* parent;
-	};
+    tree_node* root;
+    unsigned int    rotations;
 
-    TreeStruct* newTree(int key, int info, TreeStruct* parent) {
-        TreeStruct* curr = new TreeStruct;
-        curr->info = info;
-        curr->key = key;
-        curr->right = curr->left = nullptr;
-        curr->parent = parent;
-        curr->black = false;
-        return curr;
-    }
-    TreeStruct* grandparent(TreeStruct* curr) {
-        if ((curr != nullptr) && (curr->parent != nullptr))
-            return curr->parent->parent;
-        else
-            return nullptr;
-    }
-    TreeStruct* uncle(TreeStruct* curr) {
-        TreeStruct* g = grandparent(curr);
-        if (g == nullptr)
-            return nullptr; // No grandparent means no uncle
-        if (curr->parent == g->left)
-            return g->right;
-        else
-            return g->left;
-    }
-    void insert1(TreeStruct* n) {
-        if (n->parent == nullptr)
-            n->black = true;
-        else
-            insert2(n);
-
-    }
-    void insert2(TreeStruct* n) {
-        if (n->parent->black)
-            return;
-        else
-            insert3(n);
-    }
-    void insert3(TreeStruct* n) {
-        TreeStruct* u = uncle(n), * g;
-
-        if ((u != NULL) && (!u->black)) {
-            n->parent->black = true;
-            u->black = true;
-            g = grandparent(n);
-            g->black = false;
-            insert1(g);
-        }
-        else {
-            insert4(n);
-        }
-    }
-    void insert4(TreeStruct* n) {
-        TreeStruct* g = grandparent(n);
-
-        if ((n == n->parent->right) && (n->parent == g->left)) {
-            rotateLeft(n->parent);
-            n = n->left;
-        }
-        else if ((n == n->parent->left) && (n->parent == g->right)) {
-            rotateRight(n->parent);
-            n = n->right;
-        }
-        insert5(n);
-    }
-    void insert5(TreeStruct* n) {
-        TreeStruct* g = grandparent(n);
-
-        n->parent->black = true;
-        g->black = false;
-        if ((n == n->parent->left) && (n->parent == g->left)) {
-            rotateRight(g);
-        }
-        else {
-            rotateLeft(g);
-        }
-    }
-    void delete1(TreeStruct* n) {
-        if (n == node) {
-            node = nullptr;
-            return;
-        }
-        if (!n->right && !n->left) {
-            if (n->parent->right == n)
-                n->parent->right = nullptr;
+   
+    T* search(tree_node* node, T x) {
+        if (node != nullptr)
+        {
+            if (!this->m_functor->CallFunctor(t->data, x) && !this->m_functor->CallFunctor(x, t->data))
+                return (&t->data);
+            if (this->m_functor->CallFunctor(x, t->data))
+                return search(node->left, x);
             else
-                n->parent->left = nullptr;
-            return;
+                return search(node->right, x);
         }
-        else {
-            if ((n->left && n->right == nullptr) || (n->right && n->left == nullptr)) {
-                if (n->right)
-                    if (n->parent->right == n)
-                        n->parent->right = n->right;
-                    else
-                        n->parent->left = n->right;
+        return (nullptr);
+    }
+
+    int             get_color(tree_node*& node) {
+        if (node == nullptr)
+            return (BLACK);
+        return (node->color);
+    }
+
+    void            rotate_left(tree_node*& node) {
+        tree_node* right_child = node->right;
+
+        node->right = right_child->left;
+        if (node->right != nullptr)
+            node->right->parent = node;
+        right_child->parent = node->parent;
+
+        if (node->parent == nullptr)
+            root = right_child;
+        else if (node == node->parent->left)
+            node->parent->left = right_child;
+        else
+            node->parent->right = right_child;
+
+        right_child->left = node;
+        node->parent = right_child;
+        rotations++;
+    }
+
+    void            rotate_right(tree_node*& node) {
+        tree_node* left_child = node->left;
+
+        node->left = left_child->right;
+        if (node->left != nullptr)
+            node->left->parent = node;
+        left_child->parent = node->parent;
+
+        if (node->parent == nullptr)
+            root = left_child;
+        else if (node == node->parent->left)
+            node->parent->left = left_child;
+        else
+            node->parent->right = left_child;
+
+        left_child->right = node;
+        node->parent = left_child;
+        rotations++;
+    }
+
+    void            delete_tree(tree_node* node) {
+        if (node != nullptr)
+        {
+            delete_tree(node->left);
+            delete_tree(node->right);
+            delete node;
+        }
+    }
+
+    void            fix_insert(tree_node*& node) {
+        tree_node* parent = nullptr;
+        tree_node* grandparent = nullptr;
+
+        while (node != root && get_color(node) == RED && get_color(node->parent) == RED)
+        {
+            parent = node->parent;
+            grandparent = parent->parent;
+            if (parent == grandparent->left)
+            {
+                tree_node* uncle = grandparent->right;
+
+                if (get_color(uncle) == RED)
+                {
+                    set_color(uncle, BLACK);
+                    set_color(parent, BLACK);
+                    set_color(grandparent, RED);
+                    node = grandparent;
+                }
                 else
-                    if (n->parent->right == n)
-                        n->parent->right = n->left;
-                    else
-                        n->parent->left = n->left;
+                {
+                    if (node == parent->right)
+                    {
+                        rotate_left(parent);
+                        node = parent;
+                        parent = node->parent;
+                    }
+                    rotate_right(grandparent);
+                    std::swap(parent->color, grandparent->color);
+                    node = parent;
+                }
             }
             else
-                delete2(n);
+            {
+                tree_node* uncle = grandparent->left;
+
+                if (get_color(uncle) == RED)
+                {
+                    set_color(uncle, BLACK);
+                    set_color(parent, BLACK);
+                    set_color(grandparent, RED);
+                    node = grandparent;
+                }
+                else
+                {
+                    if (node == parent->left)
+                    {
+                        rotate_right(parent);
+                        node = parent;
+                        parent = node->parent;
+                    }
+                    rotate_left(grandparent);
+                    std::swap(parent->color, grandparent->color);
+                    node = parent;
+                }
+            }
         }
+        set_color(root, BLACK);
     }
-	void delete2(TreeStruct* n);
 
+    void            fix_delete(tree_node*& node) {
+        if (node == nullptr)
+            return;
+        if (node == root)
+        {
+            root = nullptr;
+            return;
+        }
+        if (get_color(node) == RED || get_color(node->left) == RED || get_color(node->right) == RED)
+        {
+            tree_node* child = node->left != nullptr ? node->left : node->right;
 
-
-
-    void rotateLeft(TreeStruct* n) {
-        TreeStruct* pivot = n->right;
-
-        pivot->parent = n->parent;
-        if (n->parent != nullptr) {
-            if (n->parent->left == n)
-                n->parent->left = pivot;
+            if (node == node->parent->left) {
+                node->parent->left = child;
+                if (child != nullptr)
+                    child->parent = node->parent;
+                set_color(child, BLACK);
+                delete node;
+            }
             else
-                n->parent->right = pivot;
+            {
+                node->parent->right = child;
+                if (child != nullptr)
+                    child->parent = node->parent;
+                set_color(child, BLACK);
+                delete node;
+            }
         }
+        else
+        {
+            tree_node* sibling = nullptr;
+            tree_node* parent = nullptr;
+            tree_node* ptr = node;
 
-        n->right = pivot->left;
-        if (pivot->left != nullptr)
-            pivot->left->parent = n;
-
-        n->parent = pivot;
-        pivot->left = n;
-        if (n == node)
-            node = pivot;
-    }
-    void rotateRight(TreeStruct* n) {
-        TreeStruct* pivot = n->left;
-        pivot->parent = n->parent; /* ïðè ýòîì, âîçìîæíî, pivot ñòàíîâèòñÿ êîðíåì äåðåâà */
-        if (n->parent != nullptr) {
-            if (n->parent->left == n)
-                n->parent->left = pivot;
+            set_color(ptr, BLACK);
+            while (ptr != root && get_color(ptr) == BLACK)
+            {
+                parent = ptr->parent;
+                if (ptr == parent->left)
+                {
+                    sibling = parent->right;
+                    if (get_color(sibling) == RED)
+                    {
+                        set_color(sibling, BLACK);
+                        set_color(parent, RED);
+                        rotate_left(parent);
+                    }
+                    else
+                    {
+                        if (get_color(sibling->left) == BLACK && get_color(sibling->right) == BLACK)
+                        {
+                            set_color(sibling, RED);
+                            if (get_color(parent) == RED)
+                                set_color(parent, BLACK);
+                            else
+                                set_color(parent, BLACK);
+                            ptr = parent;
+                        }
+                        else
+                        {
+                            if (get_color(sibling->right) == BLACK)
+                            {
+                                set_color(sibling->left, BLACK);
+                                set_color(sibling, RED);
+                                rotate_right(sibling);
+                                sibling = parent->right;
+                            }
+                            set_color(sibling, parent->color);
+                            set_color(parent, BLACK);
+                            set_color(sibling->right, BLACK);
+                            rotate_left(parent);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    sibling = parent->left;
+                    if (get_color(sibling) == RED)
+                    {
+                        set_color(sibling, BLACK);
+                        set_color(parent, RED);
+                        rotate_right(parent);
+                    }
+                    else
+                    {
+                        if (get_color(sibling->left) == BLACK && get_color(sibling->right) == BLACK)
+                        {
+                            set_color(sibling, RED);
+                            if (get_color(parent) == RED)
+                                set_color(parent, BLACK);
+                            else
+                                set_color(parent, BLACK);
+                            ptr = parent;
+                        }
+                        else
+                        {
+                            if (get_color(sibling->left) == BLACK)
+                            {
+                                set_color(sibling->right, BLACK);
+                                set_color(sibling, RED);
+                                rotate_left(sibling);
+                                sibling = parent->left;
+                            }
+                            set_color(sibling, parent->color);
+                            set_color(parent, BLACK);
+                            set_color(sibling->left, BLACK);
+                            rotate_right(parent);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (node == node->parent->left)
+                node->parent->left = nullptr;
             else
-                n->parent->right = pivot;
+                node->parent->right = nullptr;
+            delete node;
+            set_color(root, BLACK);
         }
-        n->left = pivot->right;
-        if (pivot->right != nullptr)
-            pivot->right->parent = n;
+    }
 
-        n->parent = pivot;
-        pivot->right = n;
-        if (n == node)
-            node = pivot;
+    void            set_color(tree_node*& node, int color) {
+        if (node == nullptr)
+            return;
+        node->color = color;
     }
-    void setRight(TreeStruct* curr, int key, int info) {
-        curr->right = newTree(key, info, curr);
+
+    tree_node* create_node(T val) {
+        tree_node* node = new tree_node;
+
+        if (node != nullptr)
+        {
+            node->data = val;
+            node->color = RED;
+            node->left = nullptr;
+            node->right = nullptr;
+            node->parent = nullptr;
+        }
+        return (node);
     }
-    void setLeft(TreeStruct* curr, int key, int info) {
-        curr->left = newTree(key, info, curr);
+
+    tree_node* find_min(tree_node*& node) {
+
+        tree_node* ptr = node;
+
+        while (ptr->left != nullptr)
+            ptr = ptr->left;
+
+        return ptr;
     }
+
+    tree_node* delete_bst(tree_node*& root, T data) {
+        if (root == nullptr)
+            return root;
+        if (this->comparator->compare(data, (*root).data) < 0)
+            return delete_bst(root->left, data);
+        if (this->comparator->compare(data, (*root).data) > 0)
+            return delete_bst(root->right, data);
+        if (root->left == nullptr || root->right == nullptr)
+            return root;
+
+        tree_node* temp = find_min(root->right);
+
+        root->data = temp->data;
+        return delete_bst(root->right, temp->data);
+    }
+
+    tree_node* insert_bst(tree_node*& root, tree_node*& node) {
+
+        if (root == nullptr)
+            return (node);
+
+        if (this->comparator->compare((*node).data, (*root).data) < 0)
+        {
+            root->left = insert_bst(root->left, node);
+            root->left->parent = root;
+        }
+        else if (this->comparator->compare((*node).data, (*root).data) >= 0)
+        {
+            root->right = insert_bst(root->right, node);
+            root->right->parent = root;
+        }
+        return (root);
+    }
+
 
 
 public:
-    TreeStruct* node;
-    RBTree() {
 
-        node = nullptr;
+    template typename <U>
+    Red_black_tree(U func)
+        :binary_tree<T>(func)
+    {
+        root = nullptr;
     }
-    void insert(int key, int info) {
-        TreeStruct* curr, * next;
-        if (node == nullptr) {
-            node = newTree(key, info);
-            node->black = true;
-            node->key = key;
-            node->info = info;
-        }
-        curr = next = node;
-        while (next != nullptr) {
-            curr = next;
-            if (key < curr->key)
-                next = curr->left;
-            if (key > curr->key)
-                next = curr->right;
-            if (key == curr->key) {
-                curr->info = info;
-                return;
-            }
-        }
-        if (key < curr->key) {
-            setLeft(curr, key, info);
-            next = curr->left;
-        }
-        else {
-            setRight(curr, key, info);
-            next = curr->right;
-        }
-        insert1(next);
+
+    ~Red_black_tree() {
+        if (root != nullptr)
+            delete_tree(this->root);
     }
-    void erase(int key) {
-        if (!exist(key))
-            return;
-        TreeStruct* curr, * next;
-        curr = next = node;
-        while (next != nullptr) {
-            curr = next;
-            if (key < curr->key)
-                next = curr->left;
-            if (key > curr->key)
-                next = curr->right;
-            if (key == curr->key) {
-                delete1(curr); 
-                return;
-            }
+
+    T* Find(tree_node* node, T val) {
+        return search(root, val);
+    }
+
+    bool            is_empty() {
+        return (root == nullptr);
+    }
+
+    void            clear_tree() {
+        if (root != nullptr)
+            delete_tree(root);
+        root = nullptr;
+        rotations = 0;
+    }
+
+    void Add(T x)() {
+        tree_node* node = create_node(x);
+
+        if (node != nullptr)
+        {
+            root = insert_bst(root, node);
+            fix_insert(node);
         }
     }
-    int  get(int key) {
-        TreeStruct* curr, * next;
-        curr = next = node;
-        if (exist(key))
-            while (next != nullptr) {
-                curr = next;
-                if (key < curr->key)
-                    next = curr->left;
-                if (key > curr->key)
-                    next = curr->right;
-                if (key == curr->key) {
-                    return curr->info;
-                }
-            }
+
+    void Remove(T x)() {
+        tree_node* node = delete_bst(root, x);
+
+        fix_delete(node);
     }
-    bool exist(int key) {
-        TreeStruct* curr, * next;
-        curr = next = node;
-        while (next != nullptr) {
-            curr = next;
-            if (key < curr->key)
-                next = curr->left;
-            if (key > curr->key)
-                next = curr->right;
-            if (key == curr->key) {
-                return true;
-            }
-        }
-        return false;
-    }
-    ~RBTree() {
-        delete node;
+
+    unsigned int    get_rotations() {
+        return (rotations);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
