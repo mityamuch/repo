0d53beb0f52536d36f3fdf5456b32c58f0ff3396
mityamuch <74504728+mityamuch@users.noreply.github.com>
2021-05-18 06:47:04 +0300

    preready

--- lab8.2/lab8.2.cpp
+++ lab8.2/lab8.2.cpp
@@ -1,13 +1,45 @@
 ﻿#include <iostream>
 #include <regex>
 #include "memory.h"
+#include <fstream>
+#include <map>
 
-
-
-
+using namespace std;
 int main()
 {
+	ifstream input("input.txt");
+	regex al("^(.+)=new\\((\\d+)\\)");
+	regex print("PrintVars");
+	regex open("{");
+	regex close("}");
+
+	string size;
+	getline(input, size);
+	Memory a(stoi(size));
+
+	map<string,int> vars;
+
+	try {
+		while (!input.eof()) {
+			string line;
+			getline(input, line);
+			smatch match;
+			if (regex_match(line, match, al)) {
+				
+			}
+			else if(regex_match(line, match, print)){
+			
+			}
+			else if(regex_match(line, match, open)){
+			
+			}
+			else if(regex_match(line, match, close)){
+			
+			}
 
 
-    std::cout << "Hello World!\n";
+		}
+	}
+	catch(std::exception err){ cout << err.what() << endl; }
+
 }
